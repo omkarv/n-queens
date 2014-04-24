fc0d5610a2920313d228735d7b4f9e28a87d50f1
Omkar Vedpathak <omkarv@gmail.com>
2014-04-24 08:58:08 -0700

    First attempt at n-queens, helper methods transformed and added to solvers.js

--- src/solvers.js
+++ src/solvers.js
@@ -27,12 +27,74 @@ var range = function(n) {
       });
 };
 
+var hasAnyMinorDiagonalConflicts = function(matrix, n) {
+     // var n = this.attributes.n;
+      for (var startRow = 0; startRow < n; startRow++) {
+        if(hasMinorDiagonalConflictAt(matrix, startRow, n)){
+          return true;
+        }
+      }
+      return false; // fixme
+};
+
+var hasAnyMajorDiagonalConflicts = function(matrix, n) {
+      // call the above function for all the rows
+      for (var startRow = 0; startRow < n; startRow++) {
+        if(hasMajorDiagonalConflictAt(matrix, startRow, n)){
+          return true;
+        }
+      }
+      return false; // fixme
+};
+
+var hasMinorDiagonalConflictAt = function(matrix, minorDiagonalColumnIndexAtFirstRow, n) {
+    //  var matrix = this;
+      var count;
+      for(var startCol = n - 1; startCol >= 0; startCol--) { // outer loop will change the starting col from which we will traverse diagnoally
+        count = 0;
+        for(var row = minorDiagonalColumnIndexAtFirstRow, col = startCol; row < n && col >= 0; row++, col--) {
+          if(matrix[row][col]) {
+            count++;
+          }
+        }
+        if(count > 1) {
+          return true;
+        }
+      }
+      return false; // fixme
+};
+
+var hasMajorDiagonalConflictAt = function(matrix, majorDiagonalColumnIndexAtFirstRow, n) {
+      //var matrix = this;
+      var count;
+      //there are some branches we are traversing more than once
+      //for a given start row, start on first column than iterate diagnoally as far as possible
+      //move to next column, iterate diagonally...
+      for(var startCol = 0; startCol < n; startCol++) { // outer loop will change the starting col from which we will traverse diagnoally
+        count = 0;
+        for(var row = majorDiagonalColumnIndexAtFirstRow, col = startCol; col < n && row < n && row >= 0; row++, col++) {
+         // console.log(row);
+          if(matrix[row][col]) {
+            count++;
+          }
+        }
+        if(count > 1) {
+          return true;
+        }
+      }
+      return false; // fixme
+};
+
 var innerFunction = function(n) { // generates all array combinations
+  // For each rook in each column of the top row of the to be constructed n * n matrix
+  //  iterate through the no of solutions in the n-th call of the function (since there are this many arrays possible for each rook position in the first row)
+  //      iterate through the no of rows and construct the remainder of the matrix pushing to an output
+
+  //      return a solution to the output
     var results = [];
-    if (n === 1) {
+    if (n === 1 || n === 0) {
       return [1];
-    } else {
-      
+    } else {  
       var innerSolutions = innerFunction(n-1);
       var noInnerSolutions = innerSolutions.length;  //
       for (var col = 0; col < n; col++) { // may be more than one solution per col
@@ -53,66 +115,23 @@ var innerFunction = function(n) { // generates all array combinations
                 }
                }
              }
-             results.push(result);
+             console.log('result is: ');
+             console.log(result);
+             results.push(result);  
           }
       }
       return results;
     }
-  };
+};
 
 window.findNRooksSolution = function(n) {
- // var solution = undefined; //fixme
-  //push 1 to all elements
-  // reset for each count 
- // var partial = [];// partial solutions
-  
-  // var innerFunction = function(n) { // generates all array combinations
-  //   var results = [];
-  //   if (n === 1) {
-  //     return [1];
-  //   } else {
-      
-  //     var innerSolutions = innerFunction(n-1);
-  //     var noInnerSolutions = innerSolutions.length;  //
-  //     for (var col = 0; col < n; col++) { // may be more than one solution per col
-  //         var topRow = range(col).concat([1]).concat(range(n-1-col));
-  //         for(var m = 0; m < noInnerSolutions; m++) { // iterating through inner solutions array
-  //         //iterate through no of arrays.length determined by the innerFunction(n-1)
-  //         //loop to iterate through remainder of rows and push sub arrays to output
-  //            var result = [];
-  //            result.push(topRow);
-  //            for( var i = 0 ; i < n - 1; i++) {
-  //              if(Array.isArray(innerSolutions[m])) {
-  //               result.push(innerSolutions[m][i].slice(n-col-1).concat(0).concat(innerSolutions[m][i].slice(col)));
-  //              } else {
-  //               if(col) {
-  //                  result.push(innerSolutions.concat([0]));
-  //               } else {
-  //                  result.push([0].concat(innerSolutions));
-  //               }
-  //              }
-  //            }
-  //            results.push(result);
-  //         }
-  //     }
-  //     return results;
-  //   }
-  // };
-
- // console.log(results);
-  
-  // for(var row = 0; row < n; row++){
-  //   for (var col = 0; col < n; col++) {
-  //     if(temporary) // if row conflict delete row, if 
-  //   }
-  // }
-  // console.log(innerFunction(n));
-  solution = innerFunction(n)[0];
+  var solution = innerFunction(n)[0];
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
- };
-
+};
+//it is possible to use the n-rooks solution immediately, otherwise copy n-rooks
+// and check for diagonals using a n-rooks from a base case of 4
 
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
@@ -123,11 +142,27 @@ window.countNRooksSolutions = function(n) {
   return solutionCount;
 };
 
+window.findNQueensChessboards = function(n) {
+  var rookBoards = innerFunction(n);
+  //check if has any major or minor diagonal conflicts
+  var resultArray = [];
+  for(var board = 0; board < rookBoards.length; board++) {
+    if(!hasAnyMajorDiagonalConflicts(rookBoards[board], n) && !hasAnyMinorDiagonalConflicts(rookBoards[board],n)) {
+      resultArray.push(rookBoards[board]);
+    }
+  }
+  // if neither push to results
+  return resultArray;
+
+  // if either both, investigate where, and remove, then add, if possible
+
+};  
 
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
   var solution = undefined; //fixme
+
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
@@ -137,7 +172,7 @@ window.findNQueensSolution = function(n) {
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
   var solutionCount = undefined; //fixme
-
+ 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount; // a number
 };
